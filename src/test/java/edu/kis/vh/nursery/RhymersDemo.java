05aa05e850824ae60ab8f72d219eedf7efd74d6c
michalburski251131 <251131@edu.p.lodz.pl>
2025-04-03 14:34:15 +0200

    13. Wyodrebniono test z RhymersDemo jako osobne metody

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -8,26 +8,42 @@ class RhymersDemo {
     public static void main(String[] args) {
         Rhymersfactory factory = new DefaultRhymersFactory();
 
+        testRhymers(factory);
+    }
+
+    private static void testRhymers(Rhymersfactory factory) {
         DefaultCountingOutRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
                 factory.getFIFORhymer(), factory.getHanoiRhymer()};
 
-        for (int i = 1; i < MAX_COUNT; i++)
-            for (int j = 0; j < 3; j++)
-                rhymers[j].countIn(i);
+        countInElements(rhymers);
+        countInRandomElements(rhymers);
+        printRhymerContents(rhymers);
+        printTotalRejected(rhymers);
+    }
 
-        java.util.Random rn = new java.util.Random();
-        for (int i = 1; i < MAX_COUNT; i++)
-            rhymers[3].countIn(rn.nextInt(20));
+    private static void printTotalRejected(DefaultCountingOutRhymer[] rhymers) {
+        System.out.println("total rejected is "
+                + ((HanoiRhymer) rhymers[3]).reportRejected());
+    }
 
+    private static void printRhymerContents(DefaultCountingOutRhymer[] rhymers) {
         for (int i = 0; i < rhymers.length; i++) {
             while (!rhymers[i].callCheck())
                 System.out.print(rhymers[i].countOut() + "  ");
             System.out.println();
         }
+    }
 
-        System.out.println("total rejected is "
-                + ((HanoiRhymer) rhymers[3]).reportRejected());
+    private static void countInRandomElements(DefaultCountingOutRhymer[] rhymers) {
+        java.util.Random rn = new java.util.Random();
+        for (int i = 1; i < MAX_COUNT; i++)
+            rhymers[3].countIn(rn.nextInt(20));
+    }
 
+    private static void countInElements(DefaultCountingOutRhymer[] rhymers) {
+        for (int i = 1; i < MAX_COUNT; i++)
+            for (int j = 0; j < 3; j++)
+                rhymers[j].countIn(i);
     }
 
 }
